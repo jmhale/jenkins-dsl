721075de170659ebb12fc9bd83f2480d5251d89a
James Hale <jmhale08@gmail.com>
2017-01-01 16:09:51 -0500

    Add terminate instance job

---
+++ jobs/dns_proxy_pipeline/terminate_dns_instance.groovy
@@ -0,0 +1,28 @@
+import dogsec.PythonJobBuilder
+import dogsec.common.LogRotation
+import javaposse.jobdsl.dsl.Job
+
+Job terminateDNSInstance = new PythonJobBuilder(
+  name:'terminate-dns-instance',
+  description:'Terminates the old DNS Proxy instance',
+  scriptPath:'terminate_instance.py',
+  arguments:[
+    'droplet-id':'$DROPLET_ID',
+  ],
+).build(this)
+
+terminateDNSInstance.with {
+  parameters {
+    stringParam('DROPLET_ID', null, 'ID of the Droplet to terminate')
+  }
+  scm {
+    github('jmhale/dns-proxy')
+  }
+  wrappers {
+    credentialsBinding {
+        string('DO_TOKEN', 'DigitalOcean API Key')
+    }
+  }
+}
+
+LogRotation.keepForBuilds(terminateDNSInstance, 25)
